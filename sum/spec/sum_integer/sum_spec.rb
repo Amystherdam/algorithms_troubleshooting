6f35d2137afa10cf9821faf47949b549ad2eeeb2
Amystherdam <pro.lucas07@gmail.com>
2021-01-16 17:40:15 -0300

    Modificaions to the sum challenge test file

--- sum/spec/sum_integer/sum_spec.rb
+++ sum/spec/sum_integer/sum_spec.rb
@@ -1,16 +1,17 @@
 require 'sum'
 
 describe Sum do
-  it 'error_handler_wholy_type confirm if can is Integer' do
-    calculatorx = Sum.new
-    method_instance = calculatorx.error_handler_wholy_type '10'
-    expect(method_instance).to be_instance_of(Integer)
+  context '#error_handler_integer_type' do
+    it 'blocks other types' do
+      instance = subject.error_handler_integer_type '10'
+      expect(instance).to be_instance_of(Integer)
+    end
   end
-
-  it 'number_adder accepts only Integer' do
-    calculatorx = Sum.new
-    method_instance = calculatorx.number_adder 'default', 0
-    expect(method_instance.class).to eq(Integer)
+  context '#number_adder' do
+    xit 'accepts not only other types' do
+      instance = subject.number_adder 'default'
+      expect(instance.class).to eq(Integer)
+    end
   end
 end
 
